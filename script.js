7da8459ca29406c9adc08f9c8723fe96c940ce2b
Laia Boria <lboriaa@uoc.edu>
2025-12-22 22:26:34 +0100

    modify script.js

--- script.js
+++ script.js
@@ -4,8 +4,8 @@ function showFig(src) {
   graphic.innerHTML = `<iframe src="${src}"></iframe>`;
 }
 
-// Inicial
-showFig("figs/fig1_lead_time.html");
+// Gràfic inicial
+showFig("fig1_lead_time.html");
 
 const scroller = scrollama();
 
@@ -14,14 +14,27 @@ scroller
     step: ".step",
     offset: 0.5
   })
+
   .onStepEnter(response => {
     if (response.index === 0) {
-      showFig("figs/fig1_lead_time.html");
+      showFig("fig1_lead_time.html");
     }
     if (response.index === 1) {
-      showFig("figs/fig2_adr.html");
+      showFig("fig2_adr.html");
     }
     if (response.index === 2) {
-      showFig("figs/fig3_combined.html");
+      showFig("fig3_combined.html");
+    }
+  })
+
+  .onStepExit(response => {
+    // Si pugem, mostrem el gràfic anterior
+    if (response.direction === "up") {
+      if (response.index === 2) {
+        showFig("fig2_adr.html");
+      }
+      if (response.index === 1) {
+        showFig("fig1_lead_time.html");
+      }
     }
   });
