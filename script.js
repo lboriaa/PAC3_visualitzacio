b5f2e82953fff5d5f56d24ccfb50c18171df3d5c
Laia Boria <lboriaa@uoc.edu>
2025-12-22 22:29:44 +0100

    modify script.js

--- script.js
+++ script.js
@@ -14,27 +14,30 @@ scroller
     step: ".step",
     offset: 0.5
   })
-
   .onStepEnter(response => {
+
+    // STEP 0
     if (response.index === 0) {
       showFig("figs/fig1_lead_time.html");
     }
-    if (response.index === 1) {
-      showFig("figs/fig2_adr.html");
-    }
-    if (response.index === 2) {
-      showFig("figs/fig3_combined.html");
-    }
-  })
 
-  .onStepExit(response => {
-    // Si pugem, mostrem el gràfic anterior
-    if (response.direction === "up") {
-      if (response.index === 2) {
+    // STEP 1
+    if (response.index === 1) {
+      if (response.direction === "down") {
         showFig("figs/fig2_adr.html");
       }
-      if (response.index === 1) {
+      if (response.direction === "up") {
         showFig("figs/fig1_lead_time.html");
+      }
+    }
+
+    // STEP 2
+    if (response.index === 2) {
+      if (response.direction === "down") {
+        showFig("figs/fig3_combined.html");
+      }
+      if (response.direction === "up") {
+        showFig("figs/fig2_adr.html");
       }
     }
   });
